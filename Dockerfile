145a65276de49b84166a104ffbb74e0c7b89b5ba
Sumit Asok <sumitasok@gmail.com>
2018-06-14 01:56:16 +0530

    chan

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,10 @@ MAINTAINER sumitasok@gmail.com
 RUN pip install pandas --no-cache-dir
 RUN pip install sqlalchemy --no-cache-dir
 RUN pip install pymysql --no-cache-dir
+# https://stackoverflow.com/questions/454854/no-module-named-mysqldb?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+# RUN apt install mysql-client -y
+RUN apt-get update && apt-get install -y mysql-client && rm -rf /var/lib/apt
+RUN pip install mysqlclient --no-cache-dir
 
 RUN mkdir /app
 WORKDIR /app
--- sample.py
+++ sample.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 # import time
 # time.sleep(12)
 
-mysql_conn_string = "mysql+pymysql://iamuser:P09L123cedS@mydb/iam"
+mysql_conn_string = "mysql://iamuser:P27L123pedS@mydb/iam"
 db = create_engine(mysql_conn_string)
 
 import pandas as pd
@@ -14,14 +14,14 @@ df_verses = pd.read_sql('SELECT LEAST(3, 12, 34, 8, 25)', db)
 print(df_verses)
 
 
-import SimpleHTTPServer
-import SocketServer
+# import SimpleHTTPServer
+# import SocketServer
 
-PORT = 2000
+# PORT = 2000
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+# Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+# httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-print("serving at port", PORT)
-httpd.serve_forever()
+# print("serving at port", PORT)
+# httpd.serve_forever()
